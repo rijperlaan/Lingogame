27971e39a2643ed2a64de3559c162f654db7ee2a
Robin IJperlaan <robin.ijperlaan@student.hu.nl>
2020-06-14 04:11:25 +0200

    Fix bug where guesses would get reset at the wrong time

--- src/main/resources/static/home.js
+++ src/main/resources/static/home.js
@@ -4,15 +4,37 @@ let gameTypeElement = document.getElementById("gameType");
 gameTypeElement.innerHTML = "5 letter words";
 let gameType = 5;
 
+resetGuesses();
 
 function toUpper() {
     guessField.value = guessField.value.toUpperCase();
 }
 
+function resetGuesses() {
+    let guessContainer = document.getElementById("guessContainer");
+    guessContainer.innerHTML = "";
+
+    // for (let i = 0; i < gameType; i++) {
+    //     let div = document.createElement("div");
+    //     div.classList.add("row");
+    //
+    //     for (let o = 0; o < 5; o++) {
+    //         let tile = document.createElement("div");
+    //         tile.classList.add("tile");
+    //         div.appendChild(tile);
+    //     }
+    //
+    //     guessContainer.appendChild(div)
+    // }
+}
+
 function sendGuess() {
     let word = sessionStorage.getItem("word");
+    console.log(word);
     if (word == null) {
-        initPage();
+        resetGuesses();
+        getWord();
+        return;
     }
 
     let guess = guessField.value.substring(0, gameType).toLowerCase();
@@ -32,65 +54,69 @@ function sendGuess() {
         body: JSON.stringify(data)
     };
     fetch("/api/words/guess", fetchoptions)
-        .then(function(response) {
+        .then(function (response) {
             if (response.ok) return response.json();
             else {
                 throw "Something went wrong";
             }
         })
-        .then(myJson =>  {
+        .then(myJson => {
             console.log(myJson)
-            let contentContainer = document.getElementById("contentContainer");
+            let guessContainer = document.getElementById("guessContainer");
             let div = document.createElement("div");
             div.classList.add("row");
 
+            let complete = true;
+
             for (let element of myJson) {
-                console.log(element);
                 let tile = document.createElement("div");
                 tile.classList.add("tile");
                 tile.innerHTML = element[0].toUpperCase();
                 if (element[1] === "correct") {
                     tile.classList.add("correct");
-                } else if (element[1] === "exists") {
-                    tile.classList.add("exists");
+                } else {
+                    complete = false;
+                    if (element[1] === "exists") {
+                        tile.classList.add("exists");
+                    }
                 }
                 div.appendChild(tile);
             }
-
-            contentContainer.appendChild(div);
+            if (complete) {
+                sessionStorage.clear();
+            }
+            if (guessContainer.children.length > 4) {
+                guessContainer.removeChild(guessContainer.firstChild);
+            }
+            guessContainer.appendChild(div);
             guessField.value = "";
         })
         .catch(error => console.log(error));
 }
 
-function initPage() {
-    let word = sessionStorage.getItem("word");
-    if (word == null) {
-        let fetchoptions = {
-            method: 'GET'
-        };
-        fetch("/api/words/" + gameType, fetchoptions)
-            .then(function(response) {
-                if (response.ok) return response.json();
-                else {
-                    throw "Something went wrong";
-                }
-            })
-            .then(myJson =>  {
-                console.log(myJson)
-                window.sessionStorage.setItem("word", myJson.word)
-            })
-            .catch(error => console.log(error));
-    }
+function getWord() {
+    let fetchoptions = {
+        method: 'GET'
+    };
+    fetch("/api/words/" + gameType, fetchoptions)
+        .then(function (response) {
+            if (response.ok) return response.json();
+            else {
+                throw "Something went wrong";
+            }
+        })
+        .then(myJson => {
+            window.sessionStorage.setItem("word", myJson.word);
+            sendGuess();
+        })
+        .catch(error => console.log(error));
 }
-
-initPage();
 
 guessField.addEventListener('keyup', toUpper);
 
 guessButton.addEventListener('click', sendGuess);
 
-guessField.addEventListener("keyup", function(event) {
+guessField.addEventListener("keyup", function (event) {
     // 13 is the Enter key
     if (event.keyCode === 13) {
         event.preventDefault();
